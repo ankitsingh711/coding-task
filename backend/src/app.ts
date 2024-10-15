31531b47f12b6f8eec0185953c18857f650c06d6
ankitsingh711 <developerankit2127@gmail.com>
2024-10-16 01:59:35 +0530

    added app logics and added errorHandler

---
+++ backend/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db';
+import userRoutes from './routes/userRoutes';
+import orderRoutes from './routes/orderRoutes';
+import productRoutes from './routes/productRoutes';
+import errorHandler from './middleware/errorHandler';
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/orders', orderRoutes);
+app.use('/api/v1/products', productRoutes);
+
+app.use(errorHandler);
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
